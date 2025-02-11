56953a4f225ec02872d46a3e68c1b68b03664c02
Joao Lucas <joao.lucast02@gmail.com>
2025-02-10 21:40:34 -0300

    Help, Sobre, Politica de privacidade, Termos de uso concluido

--- routes/home.php
+++ routes/home.php
@@ -9,4 +9,7 @@ $request = new Request();
 $router = new Router();
 $router->add("GET", "/", Home::homeGetPage("homeContent"));
 $router->add("GET", "/Ajuda", Home::homeGetPage("helpContent"));
+$router->add("GET", "/Privacidade", Home::homeGetPage("privacidadeContent"));
+$router->add("GET", "/Termos", Home::homeGetPage("termosContent"));
+$router->add("GET", "/Sobre", Home::homeGetPage("sobreContent"));
 $router->run($request);
